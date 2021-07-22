7226682ffc354e89b7933c127443f7fbcbec8e85
ShubhamAgrawal-13 <agrawalshubham131097@gmail.com>
2021-07-23 00:13:56 +0530

    Code Added

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Snake Game using pygame
+# Snake Game using Pygame
+
+
+Steps:
+------
+
+1. Define the board size
+2. Pygame code
+
+```python
+pygame.display.set_caption("")
+win = pygame.display.set_mode((width,height))
+clock = pygame.time.Clock()
+while(1):
+	clock.tick(10)
+	# code
+	for event in pygame.event.get():
+		if(event.type == pygame.QUIT):
+			pygame.quit()
+```
+
+3. Moving snake
+4. Check Boundary conditions
+5. Food Generation(at random)
+6. Increasing snake body and Changing snake color
+7. Score Updation
--- snake.py
+++ snake.py
@@ -4,117 +4,109 @@ import random
 width = 640
 height = 640
 
-rows = 40
-cols = 40
+rows = 20
+cols = 20
 
-block = width // rows
+bs = width // rows
 
 snake = []
-color = (122, 122, 122)
-x_dir = 1
+color = (125, 125, 125) 
+
+# right direction
+x_dir = 1  
 y_dir = 0
 
-colors = [
-		(255, 0, 0),
-		(0, 255, 0),
-		(0, 0, 255),
-		(0, 255, 255),
-		(255, 0, 255),
-		(255, 255, 0)
-]
-
 def move():
-	global snake, color, x_dir, y_dir
+	global snake, x_dir, y_dir
 	keys = pygame.key.get_pressed()
 
 	if(keys[pygame.K_LEFT]):
 		x_dir = -1
 		y_dir = 0
-		# color = (255, 0, 0)
-
 	if(keys[pygame.K_RIGHT]):
 		x_dir = 1
 		y_dir = 0
-		# color = (0, 255, 0)
-
 	if(keys[pygame.K_UP]):
 		x_dir = 0
 		y_dir = -1
-		# color = (0, 0, 255)
-
 	if(keys[pygame.K_DOWN]):
 		x_dir = 0
 		y_dir = 1
-		# color = (255, 0, 255)
+
+
 
 def draw_snake(win):
-	global snake, color
+	global snake
 	for x,y in snake:
-		pygame.draw.circle(win, color, (x*block + block//2, y*block + block//2), block//2)
-
+		pygame.draw.circle(win, color, (x*bs + bs//2, y*bs - bs//2), bs//2)
 
 def redraw(win, food):
 	win.fill((255, 255, 255))
 	draw_snake(win)
-	pygame.draw.circle(win, food[2], (food[0]*block + block//2, food[1]*block + block//2), block//2)
+	pygame.draw.circle(win, food[2], (food[0]*bs + bs//2, food[1]*bs - bs//2), bs//2)
 	pygame.display.update()
 
 def check():
 	global snake
-	x, y = snake[0]
-	# print(x, y)
-	if(x>=0 and y>=0 and x<rows and y<cols):
-		# val = snake[1:].count([x, y])
-		# if val == 0:
-		# 	return True
-		# return False
+	head = snake[0]
+	x, y = head
+	if(x>=0 and x<rows and y>=0 and y<cols):
 		return True
 	return False
 
 def generate_food():
-	global color
-	x_food = random.randint(1, rows-2)
-	y_food = random.randint(1, cols-2)
-	food_color = colors[random.randint(0, 5)]
-	return [x_food, y_food, food_color]
+	x = random.randint(1, rows-1)
+	y = random.randint(1, cols-1)
+	food_color = random_color()
+	return [x, y, food_color]
+
+def random_color():
+	x1 = random.randint(20, 230)
+	x2 = random.randint(20, 230)
+	x3 = random.randint(20, 230)
+
+	return (x1, x2, x3)
+
 
 
 if __name__ == '__main__':
-	print("Snake Game")
 	score = 0
-	pygame.display.set_caption("Snake Game : " + str(score))
+	pygame.display.set_caption("Snake Game : Score : " + str(score))
 	win = pygame.display.set_mode((width,height))
-	x = rows//2
-	y = cols//2
+	clock = pygame.time.Clock()
+	x = rows // 2
+	y = cols // 2
+
 	snake.append([x, y])
 	snake.append([x-1, y])
 	snake.append([x-2, y])
 	snake.append([x-3, y])
-	snake.append([x-4, y])
-	clock = pygame.time.Clock()
+
 	food = generate_food()
+
 	while(1):
-		clock.tick(10)
-		
+		clock.tick(7)
+		# code
+		x1 = snake[0][0] + x_dir
+		y1 = snake[0][1] + y_dir
+
+		snake.insert(0, [x1, y1]) #insert at head
+		if(x1 == food[0] and y1 == food[1]):
+			score += 10
+			color = food[2]
+			pygame.display.set_caption("Snake Game : Score : " + str(score))
+			food = generate_food()
+		else:
+			snake.pop() #remove the last entry
+
 		for event in pygame.event.get():
 			if(event.type == pygame.QUIT):
 				pygame.quit()
-		
-		x1 = snake[0][0] + x_dir
-		y1 = snake[0][1] + y_dir
-		snake.insert(0, [x1, y1])
-		if(x1 == food[0] and y1 == food[1]):
-			color = food[2]
-			score += 10
-			pygame.display.set_caption("Snake Game : " + str(score))
-			food = generate_food()
-		else:
-			snake.pop()
+
 		move()
 		redraw(win, food)
-
 		if(not check()):
+			print("Your Score : ", score)
 			print("Game Over")
-			print("your score is ", score)
 			pygame.quit()
 			break
